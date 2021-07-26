9549dc118564dd1f9d828619f57e757dde13fa6e
Roger Rodrigues de Queiroz <rogerqueiroz22@hotmail.com>
2021-07-26 15:31:17 -0300

    Others Migration

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -12,7 +12,10 @@ namespace SalesWebMvc.Models
             : base(options)
         {
         }
+        public DbSet<Department> Department { get; set; }
 
-        public DbSet<SalesWebMvc.Models.Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
+
     }
 }
